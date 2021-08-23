806c674031d02ff243d6f4a36a0ba2cee9e72223
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-23 11:24:57 +0800

    Extracted standardDelimiter() function from add()

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ export function add(input: string): number {
     if (input.substring(0, 2) === "//") {
       return customDelimiter(input);
     } else {
-      const delimiter = /\n|,/;
-      return input
-        .split(delimiter)
-        .map(Number)
-        .reduce((accumulator, currentValue) => accumulator + currentValue);
+      return standardDelimiter(input);
     }
   } else return 0;
 }
+function standardDelimiter(input: string) {
+  const delimiter = /\n|,/;
+  return input
+    .split(delimiter)
+    .map(Number)
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+
 function customDelimiter(input: string) {
   const delimiter = input.charAt(2);
   const numberedInput = input.split("\n")[1];
